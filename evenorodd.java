18daadd1c7c1ddab935e0bcbb51acee410380c33
swathi002299 <swathi002299@gmail.com>
2026-07-14 06:20:58 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,12 @@
+public class evenorodd{
+    public static void main(String[] args){
+        int num=10;
+        if(num%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+
+        }
+        }
+    }
