272d6a93cfaf3f21a259501bc8cad53711aaefa5
swathi002299 <swathi002299@gmail.com>
2026-07-28 20:03:53 +0530

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class GCD{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+            System.out.println("enter first number:");
+            int a=sc.nextInt();
+            System.out.println("enter second number");
+            int b=sc.nextInt();
+            while(b!=0){
+                int temp=b;
+                b=a%b;
+                a=temp;
+            }
+            System.out.println("GCD:"+a);
+    }
+}
---
+++ LCM.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int x = a;
+        int y = b;
+
+        // Find GCD
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+        int lcm = (a * b) / gcd;
+
+        System.out.println("LCM = " + lcm);
+
+        sc.close();
+    }
+}
